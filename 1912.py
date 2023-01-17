fdfafd99b501d9ba6c9dc3bcd7e561ef1520ca1c
SOONUK-k <kswnuguri@gmail.com>
2023-01-17 19:15:57 +0900

    230117 commit

--- 1912.py
+++ 1912.py
@@ -2,14 +2,20 @@
 N = int(input())
 arr = list(map(int, input().split()))
 
+# N=10
+# arr =[10, -4, 3, 1, 5, 6, -35, 12, 21, -1]
+
 dp = [ 0 for  _ in range(N)]
+dp[0] = arr[0]
 
 for i in range(1,N):
-    if(arr[i] < arr[i]+arr[i-1]):
-        arr[i] = arr[i]+arr[i-1]
+    if(arr[i] <= arr[i]+dp[i-1]):
+        dp[i] = arr[i]+dp[i-1]
+    else:
+        dp[i] = arr[i]
 
         
           
     
-print(arr)
-print(max(arr))
+print(dp)
+print(max(dp))
