c0f6751dcb5a99f5a27a7d4be3868ae455fc55fb
SOONUK-k <kswnuguri@gmail.com>
2023-02-05 14:24:44 +0900

    230205 commit

---
+++ 15655.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+numList = list(map(int, input().split()))
+numList.sort()
+ans=[]
+def dfs(a):
+    if len(ans)==M:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(a, len(numList)):
+        ans.append(numList[i])
+        dfs(i+1)
+        ans.pop()
+
+dfs(0)
