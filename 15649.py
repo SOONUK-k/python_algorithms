4ba95b14c9fcc9ffb12cce487be90f4b4d563d5c
SOONUK-k <kswnuguri@gmail.com>
2023-04-26 23:55:13 +0900

    230426 commit

--- 15649.py
+++ 15649.py
@@ -1,16 +1,60 @@
-n,m = list(map(int,input().split()))
- 
-s = []
- 
+
+N, M = list(map(int,input().split()))
+
+ans =[]
+bools = [0 for _ in range(N)]
+
 def dfs():
-    if len(s)==m:
-        print(' '.join(map(str,s)))
+    if len(ans)==M:
+        print(*ans)
         return
     
-    for i in range(1,n+1):
-        if i not in s:
-            s.append(i)
+    for i in range(1, N+1):
+        if not bools[i-1]:
+            ans.append(i)
+            bools[i-1] = 1
             dfs()
-            s.pop()
- 
+            ans.pop()
+            bools[i-1] = 0
+    
 dfs()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n,m = list(map(int,input().split()))
+ 
+# s = []
+ 
+# def dfs():
+#     if len(s)==m:
+#         print(' '.join(map(str,s)))
+#         return
+    
+#     for i in range(1,n+1):
+#         if i not in s:
+#             s.append(i)
+#             dfs()
+#             s.pop()
+ 
+# dfs()
